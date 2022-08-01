62ef26c93c58891d8ad4bb6fe5ec28ac55f569b4
Artem Rockatez <razvorotkapr@gmail.com>
2022-08-01 10:45:18 +0200

    add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+let numberToString = Math.abs(n).toString()
+let splitString = numberToString.split('')
+let reverseSplitString = splitString.reverse()
+let arrToString = reverseSplitString.join('')
+let stringToNumber = Number(arrToString)
+return stringToNumber
 }
